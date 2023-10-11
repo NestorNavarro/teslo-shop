f588e18003c27926c1ac2a12bd4f3f76d42d3da6
NestorNavarro <nestor.navarroarizaga@gmail.com>
2023-10-11 12:15:45 -0600

    seeds added

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/see-data';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsServices: ProductsService) {}
+
+  runSeed() {
+    this.insertNewProducts();
+    return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts() {
+    await this.productsServices.deleteAllProducts();
+    const { products } = initialData;
+
+    const insertPromises = [];
+
+    products.forEach((product) => {
+      insertPromises.push(this.productsServices.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
+    return true;
+  }
+}
